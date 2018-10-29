1671014af42dcf6a3c8f7341b3fb7b149abe41eb
RafaSalgado <rasalgadol@unal.edu.co>
2018-10-29 18:25:17 -0500

    correcion nombre de localidades

--- dao/livingcosts_dao.go
+++ dao/livingcosts_dao.go
@@ -101,7 +101,7 @@ func fulldata() {
 		"Lucero alto", "La Belleza", "Hayuelos", "El Dorado", "Ricaurte", "Santa Isabel", "Salitre", "Sierra Morena",
 		"Ciudad Bolivar", "Tunal", "Fatima", "Marsella", "Banderas", "Patio Bonito", "Aures", "Lisboa"}
 	collection := db.C(COLLECTION)
-	for j := 0; j <= 15; j++ {
+	for j := 0; j <= 45; j++ {
 		livingcost.ID = bson.NewObjectId()
 		livingcost.Zone = zonas[rand.Intn(34)]
 		livingcost.Stratification = rand.Intn(7)
